067175b07d49bf4b2019c8362626c44b565df419
JEAN-CHARLES Loïc ( freshloic) <loicjeancharles@gmail.com>
2017-12-03 17:00:59 -0400

    Modification du module NLP utilisé Spacy to Nltk

--- Phase_0.py
+++ Phase_0.py
@@ -1,48 +1,78 @@
-import spacy, bs4 as BeautifulSoup
-
-#Chargement de Spacy
+import bs4 as BeautifulSoup, nltk, time, threading, pickle 
 #Le Natural Language Processing (traitement automatique du langage naturel) 
 #a permis l’émergence d’outils de traduction automatique, de génération de texte
 #ou encore de classification de documents.
 
-nlp = spacy.load('fr')
-print("spacy chargé")
+dictionnaireWiki = {}
 
-import time
-t = time.time()
-t2 = 0
-for k in range(10):
-    dictionnaireWiki = {}
-    #len(os.listdir('page4/')) donne le nombre de fichier du répertoire
-    for NumPage in range(1,2):
+def faireDictionnaire(pageDepart,pageArrivee):
+    #listeDeCharAsuppr = ["[", "]", "'", "|", "=", "//", "/"]
+    #pageDepart = 1
+    #pageArrivee = 2
+    pageArrivee += 1
+    t = time.time()
+    
+    for NumPage in range(pageDepart,pageArrivee):
         #Ouverture en lecture de tous les fichiers du dossier des articles
-        fichierR = open('page4/'+str(NumPage)+'.txt', 'r', encoding = 'utf-8')
+        fichierR = open('pageClean/'+str(NumPage)+'.txt', 'r', encoding = 'utf-8')
         lireFichier = fichierR.read()
+        
         #Je retire les tags pour alléger le contenu
-        soup = BeautifulSoup.BeautifulSoup(lireFichier, "lxml").get_text()
+       # soup = BeautifulSoup.BeautifulSoup(lireFichier, "lxml").get_text()
         fichierR.close()
+        
         #Ouverture en écriture de tous les fichiers du dossier des articles
-        fichier = open('page4/'+str(NumPage)+'clean.txt', 'w', encoding = 'utf-8')
-        t3 = time.time()
-        document = nlp(soup)
-        t2 += time.time() - t3
-        #Je ne veux garder que les mots qui sont des noms et qui ont plus de 2 lettres
-        for mot in document:
+        #fichier = open('pageClean/'+str(NumPage)+'.txt', 'w', encoding = 'utf-8')
+        #document = nltk.word_tokenize(soup, language='french')
+        document = nltk.word_tokenize(lireFichier, language='french')
+        tags = nltk.pos_tag(document)
+        taille = len(tags) - 1
+    
+        #Je ne veux garder que les mots qui sont des noms et qui ont plus d' 1 lettre
+        for i in range(0, taille):
+            mot = tags[i][0]
+            premiereLettreTag = tags[i][1][0]
+            
+            """for char in listeDeCharAsuppr:
+                if char in mot:
+                    mot = mot.replace(char, " ")"""
+                    
             #pos [Parts of Speech] tagging detection
-            if mot.pos_ == 'NOUN' and len(mot) > 1:
-                #tokenization
-                #étape pas nécessaire pour toi, faut pas réécrire le fichier xD
-                fichier.write(mot.text+" ")
+            if  premiereLettreTag == 'N' and len(mot) > 1 :
+                #fichier.write(mot+" ")
                 #j'ajoute les mots au dictionnaires avec un tuple vide pour plusieurs clés
-                if mot.text not in dictionnaireWiki:
-                    dictionnaireWiki[mot.text] = {}
-                    dictionnaireWiki[mot.text][NumPage] = 1
+                if mot not in dictionnaireWiki:
+                    dictionnaireWiki[mot] = {}
+                    dictionnaireWiki[mot][NumPage] = 1
                 else :
-                    if NumPage in dictionnaireWiki[mot.text]:
-                        dictionnaireWiki[mot.text][NumPage] += 1
+                    if NumPage in dictionnaireWiki[mot]:
+                        dictionnaireWiki[mot][NumPage] += 1
                     else:
-                        dictionnaireWiki[mot.text][NumPage] = 1
-        fichier.close()
+                        dictionnaireWiki[mot][NumPage] = 1
+        
+        #fichier.close()
+    t = time.time()-t
+    #print(t)
 
-t = time.time()-t
-print(t)
+thread = [None] * 11
+thread[0] = threading.Thread(target = faireDictionnaire, args = (      0,    400000))
+thread[1] = threading.Thread(target = faireDictionnaire, args = (    400001,    400500))
+thread[2] = threading.Thread(target = faireDictionnaire, args = (    400501,   400700))
+thread[3] = threading.Thread(target = faireDictionnaire, args = (   400701,   400800))
+thread[4] = threading.Thread(target = faireDictionnaire, args = (   400801,   400900))
+thread[5] = threading.Thread(target = faireDictionnaire, args = (   400901,  410500))
+thread[6] = threading.Thread(target = faireDictionnaire, args = (  410501,  420500))
+thread[7] = threading.Thread(target = faireDictionnaire, args = (  420501,  440500))
+thread[8] = threading.Thread(target = faireDictionnaire, args = (  440501, 460500))
+thread[9] = threading.Thread(target = faireDictionnaire, args = ( 460501, 480500))
+thread[10] = threading.Thread(target = faireDictionnaire, args = (480501, 500000))
+
+for i in range(len(thread)):
+    thread[i].start()
+for i in range(len(thread)):
+    thread[i].join()
+    
+fichierDico = open('pageClean/dictionnaire.pickle', 'wb', encoding = 'utf-8')
+#fichierDico.write(str(dictionnaireWiki))
+pickle.dump(dictionnaireWiki,fichierDico)
+fichierDico.close()
