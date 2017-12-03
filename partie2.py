e4cc6eec05bddd0a8b5a849ce0ea5c220a40a786
JEAN-CHARLES Loïc ( freshloic) <loicjeancharles@gmail.com>
2017-12-03 17:28:34 -0400

    Ajout script nettoyage des données

---
+++ partie2.py
@@ -0,0 +1,21 @@
+
+i = 0
+
+F_xml = open('donnees.txt',encoding = 'utf-8')
+
+ligne = F_xml.readline()
+
+while i <= 500000 : # indique la fin du fichier
+    
+    F_txt = open('page4/'+str(i)+'.txt', 'w',encoding = 'utf-8')
+    
+    while not('</text' in ligne) :
+        F_txt.write(ligne)
+        ligne = F_xml.readline()
+                    
+    F_txt.write(ligne)
+    F_txt.close()
+    i += 1
+    ligne = F_xml.readline()
+    
+F_xml.close()
